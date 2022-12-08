2e2438bfcf16c0507284a7b36bda7263c2ab9130
18216878 <hugo.nsilva@csu.com.br>
2022-12-08 12:16:43 -0300

    Configurando Componente Cadastros Irregulares

--- src/app/cadastros-irregulares/cadastros-irregulares.component.ts
+++ src/app/cadastros-irregulares/cadastros-irregulares.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild, Input  } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
+import { PainelService } from '../painel.service';
+
+export interface PeriodicElement {
+  data_inicio: Date;
+  codigo: number;
+  nome: string;
+  cpf: string;
+  email: string;
+  espaco_digital: string;
+  status: string;
+  data_envio_notificacao: Date;
+  data_ultima_interacao_cn: Date;
+  orientacao_atendimento_n1: string;
+}
+
+var ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'app-cadastros-irregulares',
@@ -7,9 +27,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrosIrregularesComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'data_inicio',
+    'codigo',
+    'nome',
+    'cpf',
+    'email',
+    'espaco_digital',
+    'status',
+    'data_envio_notificacao',
+    'data_ultima_interacao_cn',
+    'orientacao_atendimento_n1'
+  ];
+  dataSource: any = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
+  
+  public pesquisa_efetuada: boolean = false;
+
+  constructor(
+    private _ngZone: NgZone, 
+    private painelService: PainelService,
+    private formBuilder: FormBuilder,
+    public dialog: MatDialog,
+    router: Router,
+    private accountService: AccountService
+  ) { 
+    this.router = router;
+    this.storage = window.localStorage;
+  }
+
+  storage: Storage;
+  router: Router;
+  formularioCadastrosIrregulares: FormGroup;
+  user: string;
+
+  public title: string = "Cadastros Irregulares";
+  public codigo_cn?: string = "";
+
+  
 
   ngOnInit(): void {
+    this.user = this.accountService.get('user')?.toString();
+    this.formularioCadastrosIrregulares = this.formBuilder.group({
+      codigo_cn:['']      
+    })
+  }
+
+  pesquisar() {
+
+    this.pesquisa_efetuada = true;
+    this.painelService.getCadastrosIrregulares(this.codigo_cn).subscribe(
+      data => {
+        this.dataSource = data;
+      }
+    )
+
+    
+  }
+
+  limpar() {
+    ELEMENT_DATA = [];
+    this.dataSource = ELEMENT_DATA;
+    this.formularioCadastrosIrregulares.reset();
+    this.pesquisa_efetuada = false;
+    this.clickedRows.clear();
   }
 
 }
